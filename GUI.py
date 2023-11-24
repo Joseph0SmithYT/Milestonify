aca721d54c0f6f91ab1a9705a6d43e0cad7542b3
Cancri (Joseph) <joeykhan0106@gmail.com>
2023-11-24 15:42:13 -0500

    Added dialog box to ask for task

--- GUI.py
+++ GUI.py
@@ -17,35 +17,59 @@ class TaskApp(wx.Frame):
 
         self.panel = wx.Panel(self)
 
-        self.amount_of_tasks_label = wx.StaticText(self.panel, label="How many tasks do you have?")
-        self.amount_of_tasks_entry = wx.TextCtrl(self.panel, style=wx.TE_PROCESS_ENTER)
+        # Set the icon
+        icon = wx.Icon("icon.ico", wx.BITMAP_TYPE_ICO)
+        self.SetIcon(icon)
 
-        self.task_label = wx.StaticText(self.panel, label="Task:")
-        self.task_entry = wx.TextCtrl(self.panel, style=wx.TE_PROCESS_ENTER)
+        # Left panel with scrolling capability
+        self.left_panel = wx.ScrolledWindow(self.panel, style=wx.VSCROLL)
+        self.left_panel.SetScrollRate(0, 20)
 
-        self.submit_button = wx.Button(self.panel, label="Submit")
+        # Task list
+        self.task_list = wx.ListCtrl(self.left_panel, style=wx.LC_REPORT)
+        self.task_list.InsertColumn(0, "Task", width=150)
+        self.task_list.InsertColumn(1, "Points", width=80)
+
+        # Right panel with input fields and buttons
+        self.right_panel = wx.Panel(self.panel)
+
+        self.amount_of_tasks_label = wx.StaticText(self.right_panel, label="How many tasks do you have?")
+        self.amount_of_tasks_entry = wx.TextCtrl(self.right_panel, style=wx.TE_PROCESS_ENTER)
+
+        self.submit_button = wx.Button(self.right_panel, label="Submit")
         self.submit_button.Bind(wx.EVT_BUTTON, self.submit_task)
 
-        self.total_points_label = wx.StaticText(self.panel, label="Total Points: 0")
+        self.total_points_label = wx.StaticText(self.right_panel, label="Total Points: 0")
 
-        self.layout = wx.BoxSizer(wx.VERTICAL)
-        self.layout.Add(self.amount_of_tasks_label, 0, wx.ALL, 5)
-        self.layout.Add(self.amount_of_tasks_entry, 0, wx.EXPAND | wx.ALL, 5)
-        self.layout.Add(self.task_label, 0, wx.ALL, 5)
-        self.layout.Add(self.task_entry, 0, wx.EXPAND | wx.ALL, 5)
-        self.layout.Add(self.submit_button, 0, wx.ALL, 5)
-        self.layout.Add(self.total_points_label, 0, wx.ALL, 5)
+        # Set up sizers for the right panel
+        right_sizer = wx.BoxSizer(wx.VERTICAL)
+        right_sizer.Add(self.amount_of_tasks_label, 0, wx.ALL, 5)
+        right_sizer.Add(self.amount_of_tasks_entry, 0, wx.EXPAND | wx.ALL, 5)
+        right_sizer.Add(self.submit_button, 0, wx.ALL, 5)
+        right_sizer.Add(self.total_points_label, 0, wx.ALL, 5)
 
-        icon = self.SetIcon(wx.Icon('icon.ico', wx.BITMAP_TYPE_ICO))
+        self.right_panel.SetSizer(right_sizer)
 
-        self.panel.SetSizer(self.layout)
+        # Set up sizer for the main panel
+        main_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        main_sizer.Add(self.left_panel, 1, wx.EXPAND)
+        main_sizer.Add(self.right_panel, 1, wx.EXPAND)
+
+        self.panel.SetSizer(main_sizer)
         self.Show()
 
     def submit_task(self, event):
         amount_of_tasks = int(self.amount_of_tasks_entry.GetValue())
-        task = self.task_entry.GetValue()
 
         while amount_of_tasks > 0:
+            dialog = wx.TextEntryDialog(self, "Enter task", "Task", style=wx.OK | wx.CANCEL)
+
+
+            if dialog.ShowModal() == wx.ID_OK:
+                task = dialog.GetValue()
+                
+
+                dialog.Destroy()
             prompt = self.starter_prompt + task
 
             # Replace the next line with your API call
@@ -57,6 +81,10 @@ class TaskApp(wx.Frame):
                 points = int(output)
                 self.total_points += points
                 amount_of_tasks -= 1
+
+                # Add task to the list
+                index = self.task_list.InsertItem(self.task_list.GetItemCount(), task)
+                self.task_list.SetItem(index, 1, str(points))
             except ValueError:
                 wx.MessageBox("AI Error", "AI made an invalid input.", wx.OK | wx.ICON_ERROR)
                 break
@@ -66,7 +94,7 @@ class TaskApp(wx.Frame):
 
 def main():
     app = wx.App(False)
-    frame = TaskApp(None, title="Milestonify", size=(400, 300))
+    frame = TaskApp(None, title="Milestonify", size=(800, 400))
     app.MainLoop()
 
 
